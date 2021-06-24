fffe2fc49b56e4722af29595d33e3ce805624cda
Francisco7777777 <83313636+Francisco7777777@users.noreply.github.com>
2021-06-24 20:43:41 -0300

    Correção

--- src/escalonador/Processador.java
+++ src/escalonador/Processador.java
@@ -6,23 +6,20 @@ public class Processador {
 
     public static void main(String[] args) throws InterruptedException {
         
-        Escalonador escalonador = new Escalonador();                   //criado o obgeto escalonador.
+        Escalonador escalonador = new Escalonador();                 
         Processo.escalonador = escalonador;
-        Escalonador.qtdeProcesso = 4;                        //escalonador recebe a quantidade de processos
+        Escalonador.qtdeProcesso = 4;                      
         
-        //cria o objeto processo(1, 2, 3, 4) é suas caracteristicas.
         Processo p1 = new Processo("NetBeans", EnumTipo.CPU, 4, 2);
         Processo p2 = new Processo("QAcademico", EnumTipo.IO, 6, 2);
         Processo p3 = new Processo("Familydoc", EnumTipo.CPU, 3, 2);
         Processo p4 = new Processo("SOAE", EnumTipo.IO, 4, 2);
-        
-        //os processo estao esta recebendo uma lista de processos prontos.
+     
         Escalonador.prontos.add(p1);
         Escalonador.prontos.add(p2);
         Escalonador.prontos.add(p3);
         Escalonador.prontos.add(p4);
         
-        //O escalonador escalona o prosesso.
         escalonador.escalonarProcesso();
 
         p1.start();
@@ -35,7 +32,6 @@ public class Processador {
         p3.join();
         p4.join();
         
-        //chama o metodo imprimir status da lista.
         Escalonador.imprimeStatusListas();
 
     }
