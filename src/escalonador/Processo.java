e4e8aa8689b86a84690c2c89de23e3e7e0ed5f0d
Francisco7777777 <83313636+Francisco7777777@users.noreply.github.com>
2021-06-24 20:47:11 -0300

    Correção

--- src/escalonador/Processo.java
+++ src/escalonador/Processo.java
@@ -7,15 +7,15 @@ import java.util.logging.Logger;
 
 public class Processo extends Thread {
 
-    public int pc = 0;                           //Armasena as informaçoes processadas de quada processo.
+    public int pc = 0;                           
     public String nome;
     public EnumEstado estado = EnumEstado.PRONTO;
     public EnumTipo tipo;
     public int qtdePc;
     public static Escalonador escalonador;
-    public int quantum;                         //Intervalo de tempo entre cada interupição.
+    public int quantum;                         
 
-    public Processo(String nome, EnumTipo tipo, int qtdePc, int quantum) {  //Classe construtora.
+    public Processo(String nome, EnumTipo tipo, int qtdePc, int quantum) { 
         this.nome = nome;
         this.tipo = tipo;
         this.qtdePc = qtdePc;
@@ -24,21 +24,19 @@ public class Processo extends Thread {
 
     @Override
     public void run() {
-        //Enquanto a quatidade de atividade do processo for mair do que as atividade processadas.
+        
         while (this.qtdePc > this.pc) {
             try {
-                //Se o estado do processo escalonado for "EXECULTANDO".
+               
                 if (this.estado.equals(EnumEstado.EXECUTANDO)) {
                         
-                        /*Equanto o valor da variavel(i) for menor que a que o valor da variavel(quantum)
-                        é a quantidade de tarefas for maior as  que as informações processadada */
                         for (int i = 0; i < this.quantum && this.qtdePc > this.pc; i++) {
                             this.pc++;
                         }
                     this.sleep(50);
-                    escalonador.escalonarProcesso(); //O escalonador escalona um processo.
+                    escalonador.escalonarProcesso();
                 
-                } else {   //um processo e imterompido é o escalonador escalona um processo no topo da fila. 
+                } else {   
                     synchronized (this) {
                         wait();
                     }
@@ -51,8 +49,6 @@ public class Processo extends Thread {
 
     @Override
     public String toString() {
-        
-        //Retorna o nome do processo, quantidade de "pc", é "qtdePc" 
         return this.nome + "(" + this.pc + "/" + this.qtdePc + ")";
     }
 
